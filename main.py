694f6fcf8783a045a7ce6599f88fba09ae4c363a
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-11-08 03:34:06 +0700

    the basis

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QApplication
+import sys
+
+from main_window import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
